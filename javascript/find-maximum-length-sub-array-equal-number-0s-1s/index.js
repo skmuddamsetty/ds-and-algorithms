1dd771cd085ae3483fdd842665edb67c2b7cbdc4
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-18 22:08:01 -0400

    find-maximum-length-sub-array-equal-number-0s-1s

---
+++ javascript/find-maximum-length-sub-array-equal-number-0s-1s/index.js
@@ -0,0 +1,36 @@
+/**
+ * https://www.techiedelight.com/find-maximum-length-sub-array-equal-number-0s-1s/
+ * @param {Array} A
+ */
+const findMaxLengthSubArrayWithEqualZerosAndOnes = (A) => {
+  let map = { 0: -1 };
+  let index = 0;
+  let currSum = 0;
+  let maxLength = -1;
+  let highIndex;
+  let lowIndex;
+  for (let num of A) {
+    if (num === 0) {
+      currSum = currSum - 1;
+    } else {
+      currSum = currSum + 1;
+    }
+    if (map[currSum] !== undefined && maxLength < index - map[currSum]) {
+      maxLength = index - map[currSum];
+      highIndex = index;
+      lowIndex = map[currSum] + 1;
+    }
+    if (map[currSum] === undefined) {
+      map[currSum] = index;
+    }
+    index++;
+  }
+  console.log(
+    `Max length sub array with equal number of 1\'s and 0\'s [${lowIndex},${highIndex}] ${maxLength}`
+  );
+};
+
+findMaxLengthSubArrayWithEqualZerosAndOnes([0, 0, 1, 0, 1, 0, 0]);
+findMaxLengthSubArrayWithEqualZerosAndOnes([1, 0, 1, 0, 1, 0, 0]);
+findMaxLengthSubArrayWithEqualZerosAndOnes([0, 0, 1, 0, 1, 0, 1]);
+module.exports = { findMaxLengthSubArrayWithEqualZerosAndOnes };
