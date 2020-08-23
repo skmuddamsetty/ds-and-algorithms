87015caeebe65eb8ef06e4d0bcc0c23239459fb2
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-23 09:47:30 -0400

    created trees

--- javascript/binary-tree-problems/shared/test.js
+++ javascript/binary-tree-problems/shared/test.js
@@ -5,17 +5,65 @@ const {
 
 let x;
 let y;
+let z;
 beforeEach(() => {
   // prepare x tree
-  x = new Node(1);
-  x.left = new Node(2);
-  x.right = new Node(3);
+  /* Construct below Tree
+             15
+            /  \
+           /    \
+          10     20
+         / \     / \
+        8   12  16  25
+  */
+  x = new Node(15);
+  x.left = new Node(10);
+  x.right = new Node(20);
+  x.left.left = new Node(8);
+  x.left.right = new Node(12);
+  x.right.left = new Node(16);
+  x.right.right = new Node(25);
   // prepare y tree
-  y = new Node(1);
-  y.left = new Node(2);
-  y.right = new Node(3);
+  /* Construct below Tree
+             15
+            /  \
+           /    \
+          10     20
+         / \     / \
+        8   12  16  25
+                      \
+                       \
+                       32
+  */
+  y = new Node(15);
+  y.left = new Node(10);
+  y.right = new Node(20);
+  y.left.left = new Node(8);
+  y.left.right = new Node(12);
+  y.right.left = new Node(16);
+  y.right.right = new Node(25);
+  y.right.right.right = new Node(32);
+  // prepare z tree
+  /* Construct below Tree
+              1
+            /  \
+           /    \
+          2      3
+         / \      \
+        4   5      6
+                  / \
+                 7   8
+  */
+  z = new Node(1);
+  z.left = new Node(2);
+  z.right = new Node(3);
+  z.left.right = new Node(4);
+  z.right.left = new Node(5);
+  z.right.right = new Node(6);
+  z.right.left.left = new Node(7);
+  z.right.left.right = new Node(8);
 });
 
 it('checkIfTwoBinaryTreesIdentical_iterative:should tell if two given trees are identical or not', () => {
-  checkIfTwoBinaryTreesIdentical_iterative(x, y);
+  expect(checkIfTwoBinaryTreesIdentical_iterative(x, y)).toBe(true);
 });
