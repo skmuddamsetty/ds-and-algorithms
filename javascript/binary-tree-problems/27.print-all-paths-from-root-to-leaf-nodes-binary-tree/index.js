7cbc27ec454dd1976d3db0cd733e233dfadbb0e1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-25 14:34:03 -0400

    added comments to print-all-paths-from-root-to-leaf-nodes-binary-tree

--- javascript/binary-tree-problems/27.print-all-paths-from-root-to-leaf-nodes-binary-tree/index.js
+++ javascript/binary-tree-problems/27.print-all-paths-from-root-to-leaf-nodes-binary-tree/index.js
@@ -1,7 +1,10 @@
 const { a } = require('../shared/tree');
 /**
- *
+ * https://www.techiedelight.com/print-all-paths-from-root-to-leaf-nodes-binary-tree/
  * @param {Node} root
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(h) - extra space for the call stack where h is the height of the tree
  */
 const printAllRootToLeafPaths = (root) => {
   helper(root);
