a10df0966b31b4fe51863fb79c90fdd5b5308e43
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-08-27 22:46:35 -0400

    sort-array-containing-0s-1s-2s-dutch-national-flag-problem

--- javascript/array-problems/9.sort-array-containing-0s-1s-2s-dutch-national-flag-problem/index.js
+++ javascript/array-problems/9.sort-array-containing-0s-1s-2s-dutch-national-flag-problem/index.js
@@ -1,17 +1,30 @@
+/**
+ * https://www.techiedelight.com/sort-array-containing-0s-1s-2s-dutch-national-flag-problem/
+ *
+ * @param {Array} A
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 const sortThreeNumbers = (A) => {
   let start = 0;
   let mid = 0;
   let pivot = 1;
   let end = A.length - 1;
   while (mid <= end) {
+    // current element is 0
     if (A[mid] < pivot) {
       swap(A, start, mid);
       start++;
       mid++;
-    } else if (A[mid] > pivot) {
+    }
+    // current element is 2
+    else if (A[mid] > pivot) {
       swap(A, mid, end);
       end--;
-    } else {
+    }
+    // current element is 1
+    else {
       mid++;
     }
   }
